6e8befa5988d2ae4dda1b8c93b50a940765b0aa1
Dushyant Ramful <dushyantramful@gmail.com>
2021-07-27 16:53:13 +0400

    basic completed need css

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,42 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+
   def index
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
   def new
-    @article = Article.find(params[:id])
+    @article = Article.new
   end
 
   def create
-    @article = Article.find(params[:id])
+    @article = Article.new(articles_params)
     @article.save
     redirect_to articles_path
   end
+
+  def edit; end
+
+  def update
+    # @article = Article.find(params[:id])
+    @article.update
+    redirect_to articles_path
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def articles_params
+    params.require(:article).permit(:title, :context)
+  end
+
 end
