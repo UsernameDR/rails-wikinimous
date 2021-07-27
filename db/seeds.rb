1c92a3928b6163a765e64372c296b850b19d77f5
Dushyant Ramful <dushyantramful@gmail.com>
2021-07-27 15:46:16 +0400

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,12 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  title = Faker::Artist.name
+  context = Faker::Quote.famous_last_words
+  article = Article.new(title: title, context: context)
+  article.save
+end
